3a734d5b52ffcfd1a1170513336a2621dae90781
Calcium <62979683+C-alcium@users.noreply.github.com>
2020-12-25 14:49:24 +0000

    Fix up API type to allow using any of the given parameters

--- lib/NewsAPI.hs
+++ lib/NewsAPI.hs
@@ -1,15 +1,23 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE TypeOperators     #-}
 
 module NewsAPI
   ( runEverything
+  , EverythingQueryParameters (..)
+  , defaultEverythingParams 
   ) where
 
 
+import           Control.Monad.IO.Class
 import           Data.Aeson
+import           Data.List               (intercalate)
+import           Data.Maybe
 import           Data.Proxy
+import           Data.Time
+import           Data.Time.Clock
 import           GHC.Generics
 import           Network.HTTP.Client     (newManager)
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
@@ -36,25 +44,87 @@ data ArticleResult = ArticleResult { source      :: Source,
 data Source = Source { id   :: String,
                        name :: String } deriving (Show, Eq, Generic)
 
+data EverythingQueryParameters = EverythingQueryParameters
+  { apiKey         :: Maybe String,
+    q              :: Maybe String,
+    qInTitle       :: Maybe String,
+    sources        :: Maybe [String],
+    domains        :: Maybe [String],
+    excludeDomains :: Maybe [String],
+    from           :: Maybe Day,
+    to             :: Maybe Day,
+    language       :: Maybe String,
+    sortBy         :: Maybe SortType,
+    pageSize       :: Maybe Int,
+    page           :: Maybe Int } deriving (Show, Eq, Generic)
+
+data SortType = Relevancy
+              | Popularity
+              | PublishedAt deriving (Show, Eq, Generic)
+
 instance FromJSON NewsResult
 instance FromJSON ArticleResult
 instance FromJSON Source
+instance FromJSON SortType
+
+
+defaultEverythingParams :: EverythingQueryParameters
+defaultEverythingParams = EverythingQueryParameters 
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
+    Nothing
 
 type FilterString = String
 type APIKey       = String
+type CSVString    = String
 
 type NewsAPI = "v2/everything"
-  :> QueryParam "q" String
   :> QueryParam "apiKey" String
+  :> QueryParam "q" String
+  :> QueryParam "qInTitle" String
+  :> QueryParam "sources" CSVString
+  :> QueryParam "domains" CSVString
+  :> QueryParam "excludeDomains" CSVString
+  :> QueryParam "from" Day
+  :> QueryParam "to" Day
+  :> QueryParam "language" String
+  :> QueryParam "sortBy" String
+  :> QueryParam "pageSize" Int
+  :> QueryParam "page" Int
   :> Get '[JSON] NewsResult
+
 
 newsAPI :: Proxy NewsAPI
 newsAPI = Proxy
 
 everything = client newsAPI
 
-runEverything :: APIKey -> FilterString -> IO (Either ClientError NewsResult)
-runEverything key term = do
-   manager' <- newManager tlsManagerSettings
-   runClientM (everything (Just term) (Just key)) (mkClientEnv manager' (BaseUrl Http apiRoot 80 ""))
+runEverything :: EverythingQueryParameters -> IO (Either ClientError NewsResult)
+runEverything (EverythingQueryParameters apiKey q qInTitle sources domains excludeDomains from to language sortBy pageSize page) = do
+  manager'     <- newManager tlsManagerSettings
+  producedFrom <- produceDate from
+  producedTo   <- produceDate to
+  runClientM (everything apiKey q qInTitle sourceCSV domainCSV excludeCSV (Just producedFrom) (Just producedTo) language sortType pageSize page) (mkClientEnv manager' (BaseUrl Http apiRoot 80 ""))
+    where
+      sourceCSV = sources >>= toCSVString
+      domainCSV = domains >>= toCSVString
+      excludeCSV = excludeDomains >>= toCSVString
+      sortType = sortBy >>= (Just . show)
+
+produceDate :: Maybe Day -> IO Day
+produceDate Nothing  = utctDay <$> getCurrentTime
+produceDate (Just d) = pure d
+
+toCSVString :: [String] -> Maybe CSVString
+toCSVString [] = Nothing
+toCSVString xs = Just (intercalate "," xs)
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
-import qualified NewsAPI            as NAPI
-
+import           NewsAPI
 import           System.Environment
 
 main :: IO ()
 main = do
   newsapikey <- getEnv "NEWS_API_KEY"
-  res <- NAPI.runEverything newsapikey "bitcoin"
-  case res of
-    (Left e)   -> print e
-    (Right r ) -> print r
+  let mApiKey = Just newsapikey
+  let mSearch = Just "bitcoin"
+  let eParams = defaultEverythingParams { apiKey = mApiKey, q = mSearch}
+  res <- runEverything eParams
+  print res
+
