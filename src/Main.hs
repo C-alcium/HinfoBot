b30abc27efa6b5a96173f2cbf6f3c1523d9139e4
Calcium <62979683+C-alcium@users.noreply.github.com>
2020-12-27 02:28:57 +0000

    Initial commit of command system

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,19 @@
 
 module Main where
 
-import           Control.Monad      (unless)
+import           Control.Monad        (when)
 import           Data.Either
-import           Data.Text          as T
-import           Data.Text.IO       as TIO
+import qualified Data.List.Split      as S
+import           Data.Text            as T
+import           Data.Text.IO         as TIO
+import           Data.Void
 import           Discord
-import qualified Discord.Requests   as DR
+import qualified Discord.Requests     as DR
 import           Discord.Types
 import           NewsAPI
 import           System.Environment
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
 
 main :: IO ()
 main = do
@@ -23,12 +27,49 @@ main = do
     }
   TIO.putStrLn userFacingError
 
-messageHandler event = case event of
-  MessageCreate m -> unless (fromBot m) $ do
-      _ <- restCall (DR.CreateMessage (messageChannel m) "Ping")
-      pure ()
-  _ -> pure ()
+--------------- Event Handling Code ---------------
 
+messageHandler event = case event of
+  MessageCreate m -> when (not (fromBot m) && isCommandMessage m) $ do
+      let commandParseResult = applyCommandParse (messageText m)
+      performCommandAction commandParseResult m
+  _ -> pure ()
+  where
+    applyCommandParse m = runParser pCommand "" (T.unpack m)
+
+--------------- Command Code ---------------
+
+performCommandAction (Left _)                 _ = pure ()
+performCommandAction (Right (vCommand, args)) m =
+  case vCommand of
+    Help -> pure ()
+    Ping -> do
+      _ <- restCall (DR.CreateMessage (messageChannel m) "Pong")
+      pure ()
+
+--------------- Command Predicates ---------------
+
+commandPrefix :: Text
+commandPrefix = "&"
 
 fromBot :: Message -> Bool
 fromBot m = userIsBot (messageAuthor m)
+
+isCommandMessage :: Message -> Bool
+isCommandMessage m = commandPrefix `isPrefixOf` messageText m
+
+--------------- Parse Command ---------------
+
+type Parser = Parsec Void String
+
+data ValidCommand = Help
+                  | Ping deriving (Eq, Show)
+
+pCommand :: Parser (ValidCommand, [String])
+pCommand = do
+  _             <- char '&'
+  parsedCommand <- choice [ Help <$ string' "help"
+                          , Ping <$ string' "ping" ]
+  arguments     <- manyTill anySingle eof
+  pure (parsedCommand, S.splitOn " " arguments)
+
