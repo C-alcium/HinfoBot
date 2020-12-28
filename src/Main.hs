c8401414915a67d88ee215f0a2cc0f123e312807
Calcium <62979683+C-alcium@users.noreply.github.com>
2020-12-28 17:45:35 +0000

    Make it so that search results are actually displayed

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
 
 
 module Main where
 
 import           Control.Monad              (when)
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Reader
-import Control.Monad.IO.Class
+import Data.Maybe (isJust, fromJust)
 import           Data.Either
 import qualified Data.List.Split            as S
 import           Data.Text                  as T
 import           Data.Text.IO               as TIO
+import           Data.Typeable
 import           Data.Void
-import Data.Typeable
 import           Discord
 import qualified Discord.Requests           as DR
 import           Discord.Types
@@ -44,12 +45,14 @@ messageHandler event = case event of
 
 --------------- Command Execution ---------------
 
+-- Hook for determining which command to execute 
+
 performCommandAction (Left _)                 _ = pure ()
 performCommandAction (Right (vCommand, args)) m =
   case vCommand of
-    Help -> executeHelpCommand m
-    Ping -> executePingCommand m
-    Search -> executeSearchCommand m args 
+    Help   -> executeHelpCommand m
+    Ping   -> executePingCommand m
+    Search -> executeSearchCommand m args
 
 -- Ping
 
@@ -64,17 +67,17 @@ executeHelpCommand :: Message -> ReaderT DiscordHandle IO ()
 executeHelpCommand m = do
   _ <- restCall (DR.CreateMessageEmbed (messageChannel m) "" $
     def { createEmbedTitle       = "Help Menu :: HinfoBot"
-        , createEmbedDescription = "The basic functionality of each command is described below" 
+        , createEmbedDescription = "The basic functionality of each command is described below"
         , createEmbedFields      = commandDescriptions
         })
   pure ()
 
 commandDescriptions :: [EmbedField]
 commandDescriptions = Prelude.map describeCommand commandList
-  where 
-    describeCommand c = EmbedField (T.pack  (show c)) (describe c) Nothing
+  where
+    describeCommand c = EmbedField (tShow c) (describe c) Nothing
 
--- Search 
+-- Search
 
 executeSearchCommand :: Message -> [String] -> ReaderT DiscordHandle IO ()
 executeSearchCommand m args = do
@@ -82,11 +85,31 @@ executeSearchCommand m args = do
   let target = messageChannel m
   searchRes <- liftIO (search' glued)
   _         <- case searchRes of
-                 Left  e -> sendMessageOrError target (T.pack ("API Call failed: " <> show e))
-                 Right a -> sendMessageOrError target ((T.pack .show . totalResults) a)
+                 Left e -> sendMessageOrError target (T.pack ("API Call failed: " <> show e))
+                 Right a -> do
+                   _ <- restCall (DR.CreateMessageEmbed (messageChannel m) "" (buildSearchEmbed a (T.pack glued)))
+                   pure ()
+
   pure ()
 
--- Search the API 
+
+buildSearchEmbed :: NewsResult -> Text -> CreateEmbed
+buildSearchEmbed result query = 
+  def { createEmbedTitle       = "Results for" <> query
+      , createEmbedDescription = "found" <> tShow (totalResults result) <> " result(s)" 
+      , createEmbedFields      = produceResultFields (articles result)
+      }
+
+
+
+produceResultFields :: [ArticleResult] -> [EmbedField]
+produceResultFields articles = Prelude.map produceField (validArticles articles)
+  where
+    validArticles as     = Prelude.filter (\ a -> isJust (title a) && isJust (url a)) as
+    produceField article = EmbedField ((T.pack . fromJust) (title article)) ((T.pack . fromJust) (url article)) Nothing
+
+
+-- Search the API
 
 search' query = do
  newsapikey <- getEnv "NEWS_API_KEY"
@@ -103,7 +126,6 @@ sendMessageOrError target message = do
       pure ()
     (Right v) -> pure ()
 
-
 --------------- Command Predicates ---------------
 
 commandPrefix :: Text
@@ -119,12 +141,12 @@ isCommandMessage m = commandPrefix `isPrefixOf` messageText m
 
 type Parser = Parsec Void String
 
-data ValidCommand = Help 
-                  | Ping 
+data ValidCommand = Help
+                  | Ping
                   | Search deriving (Eq, Show, Ord, Enum, Bounded)
 
 class ExecutableCommand x where
-  describe :: x -> Text 
+  describe :: x -> Text
 
 instance ExecutableCommand ValidCommand where
   describe Help   = "Displays this help menu"
@@ -135,7 +157,7 @@ commandList :: [ValidCommand]
 commandList = enumFrom minBound :: [ValidCommand]
 
 validCommandP :: Parser ValidCommand
-validCommandP = choice $ Prelude.map build' commandList 
+validCommandP = choice $ Prelude.map build' commandList
   where
     build' a = a <$ string' (show a)
 
@@ -145,4 +167,9 @@ pCommand = do
   parsedCommand <- validCommandP
   arguments     <- manyTill anySingle eof
   pure (parsedCommand, S.splitOn " " arguments)
+
+--------------- Utility ---------------
+
+tShow :: (Show a) => a -> Text
+tShow = T.pack . show 
 
